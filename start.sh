9777aa0355e507013a54286ab3ce6d26cbf9a6d6
mluu <mluu@us.ibm.com>
2014-03-28 16:20:15 -0700

    fix chmod calls

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # when using docker mounted volumes, the owner/group is set to root
 if [ `stat --format=%U /var/lib/cassandra` != "cassandra" ] ; then
-  chown -R /var/lib/cassandra cassandra:cassandra
+  chown -R cassandra:cassandra /var/lib/cassandra
 fi
 if [ `stat --format=%U /var/log/cassandra` != "cassandra" ] ; then
-  chown -R /var/log/cassandra cassandra:cassandra
+  chown -R cassandra:cassandra /var/log/cassandra
 fi
 /usr/sbin/cassandra -f
