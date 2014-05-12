2ec00f499a7e9ed52a0773ce98524c2a2a0ffd52
mluu <mluu@lancope.com>
2014-05-12 13:22:31 -0700

    set listen_address and seeds to container's IP

--- start.sh
+++ start.sh
@@ -6,4 +6,7 @@ fi
 if [ `stat --format=%U /var/log/cassandra` != "cassandra" ] ; then
   chown -R cassandra:cassandra /var/log/cassandra
 fi
+IP=$(ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+sed -i -e "s/^\(listen_address:\).*/\1 $IP/" /etc/cassandra/cassandra.yaml
+sed -i -e "s/^\([ ]*- seeds:\).*/\1 $IP/" /etc/cassandra/cassandra.yaml
 start-stop-daemon --chuid cassandra:cassandra --exec /usr/sbin/cassandra --start -- -f
