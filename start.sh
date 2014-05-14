ef8f0a9da1067db223ae9ab1492ac7e1118afe58
mluu <mluu@lancope.com>
2014-05-14 11:15:49 -0700

    allow setting cassandra ip via env var

--- start.sh
+++ start.sh
@@ -6,7 +6,9 @@ fi
 if [ `stat --format=%U /var/log/cassandra` != "cassandra" ] ; then
   chown -R cassandra:cassandra /var/log/cassandra
 fi
-IP=$(ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
-sed -i -e "s/^\(listen_address:\).*/\1 $IP/" /etc/cassandra/cassandra.yaml
-sed -i -e "s/^\([ ]*- seeds:\).*/\1 $IP/" /etc/cassandra/cassandra.yaml
+if [ -z "$CASSANDRA_IP" ] ; then
+  CASSANDRA_IP=$(ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+fi
+sed -i -e "s/^\(listen_address:\).*/\1 $CASSANDRA_IP/" /etc/cassandra/cassandra.yaml
+sed -i -e "s/^\([ ]*- seeds:\).*/\1 $CASSANDRA_IP/" /etc/cassandra/cassandra.yaml
 start-stop-daemon --chuid cassandra:cassandra --exec /usr/sbin/cassandra --start -- -f
